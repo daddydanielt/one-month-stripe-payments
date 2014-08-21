aec37d248651ad507b0c8d8057f85d8982d5e066
DaddyDanielT <artsppdaniel@gmail.com>
2014-08-21 21:55:31 +0800

    modified

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,19 +1,20 @@
 class ChargesController < ApplicationController
-
-
+  @charge_test
   def create
-  # Amount in cents
-  @amount = 500
-    customer = Stripe::Customer.create(
-      :email => 'example@stripe.com',
+
+  # Amount in cents  
+    customer = Stripe::Customer.create(      
+      :email => params[:stripeEmail],
       :card  => params[:stripeToken]
     )
+
     charge = Stripe::Charge.create(
       :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
+      :amount      => params[:amount],
+      :description => 'Growth Hacking Crash Course',
       :currency    => 'usd'
     )
+  
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to charges_path
